efe46dff382cdbe8e9a661ee71f6fad91fb7834f
Renan Tavares <renan_velox@hotmail.com>
2017-10-15 18:48:03 -0300

    criando o metodo encontraJogador

--- PacMan.rb
+++ PacMan.rb
@@ -6,6 +6,19 @@ def leMapa(numero)
     mapa = texto.split "\n"
 end
 
+def encontraJogador
+    for linha = 0..(mapa.size-1)
+        if mapa[linha].include? "H"
+            for coluna = 0..(mapa[linha].size-1)
+                if mapa[linha] [coluna] == "H"
+                    #encontrei o jogador
+                end
+            end
+        end
+    end
+    #nao encontrei o jogador
+end
+
 def joga(nome)
    mapa = leMapa(1)
 
