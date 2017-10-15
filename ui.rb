9528af73b93127cd58743a4a0b00f92c96efe934
Renan Tavares <renan_velox@hotmail.com>
2017-10-15 18:23:55 -0300

    criando as funçoes boasVindas, desenha, pedeMovimento

---
+++ ui.rb
@@ -0,0 +1,17 @@
+def boasVindas
+    puts "bem vindo ao jogo do Pac Man"
+    puts "qual é seu nome?"
+    nome = gets.strip
+    puts "começaremos o jogo pra voce #{nome}"
+    puts "\n\n\n\n\n"
+    nome
+end
+
+def desenha(mapa)
+    puts mapa
+end
+
+def pedeMovimento
+    puts "Para onde deseja ir?"
+    movimento = gets.strip
+end
